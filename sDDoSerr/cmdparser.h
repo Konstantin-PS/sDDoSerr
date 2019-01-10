422f286a7706d88ad7f694e43ba0d788c02d7bf5
Konstantin-PS <konstantin.p.96@gmail.com>
2019-01-10 23:04:11 +0300

    v.1.1.2a от 10.01.2019. Начата разработка. Написан модуль сдвоенного парсера командной строки и ini-файлов (на основе minIni).

--- sDDoSerr/cmdparser.h
+++ sDDoSerr/cmdparser.h
@@ -1,23 +1,73 @@
 /*
+ * sDDoSerr - the programm for simulate shrew (D)DoS attack.
+ * 
  * Заголовочный файл для парсера командной строки.
- * Попытка.
+ * 
+ * v.1.1.2a от 10.01.19.
  */
+ 
+/**
+    This file is part of sDDoSerr.
+sDDoSerr is a research program for emulating shrew (D)DoS traffic and
+its analysis (in development).
+Use this program on your own pril and risk, as with improper use 
+there is a risk of disruption of the network infrastucture.
+DDoSerr Copyright © 2019 Konstantin Pankov 
+(e-mail: konstantin.p.96@gmail.com), Mikhail Riapolov.
+
+    sDDoSerr is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Any distribution and / or change must be agreed with the authors and
+    is prohibited without their permission.
+    At this stage of the program development, authors are forbidden to 
+    embed any of DDoSerr modules (code components) into other programs.
+
+    sDDoSerr is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with sDDoSerr.  If not, see <https://www.gnu.org/licenses/>.
+
+
+    Этот файл — часть sDDoSerr.
+sDDoSerr — это исследовательская программа для эмуляции "shrew" (D)DoS 
+трафика и его анализа (в разработке). 
+Используйте эту программу на свой страх и риск, так как при неправильном
+применении есть риск нарушения работы сетевой инфраструктуры.
+sDDoSerr Copyright © 2019 Константин Панков 
+(e-mail: konstantin.p.96@gmail.com), Михаил Ряполов.
+
+   sDDoSerr - свободная программа: вы можете перераспространять ее и/или
+   изменять ее на условиях Стандартной общественной лицензии GNU
+   в том виде, в каком она была опубликована 
+   Фондом свободного программного обеспечения; либо версии 3 лицензии, 
+   либо (по вашему выбору) любой более поздней версии.
+
+   Любое распространиение и/или изменение должно быть согласовано с
+   авторами и запрещается без их разрешения.
+   На данном этапе развития программы авторами запрещается встраивать 
+   любой из модулей (компонентов кода) DDoSerr в другие программы.
+
+   sDDoSerr распространяется в надежде, что она будет полезной,
+   но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+   или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+   общественной лицензии GNU.
+
+   Вы должны были получить копию Стандартной общественной лицензии GNU
+   вместе с этой программой. Если это не так, см.
+   <https://www.gnu.org/licenses/>. 
+ **/ 
 
 #ifndef CMDPARSER_H
 #define CMDPARSER_H
 
-//const char config[];
-
-/*
-//Внутренние переменные со значениями из конфиг-файла.
-int size; //Размер пакета.
-int buffsize; //Размер буффера передачи.
-int protocol; //Протокол.
-int procnum; //Количество процессов/потоков.
-*/
-
 //Другие модули должны знать только об структурах с настройками и 
-//функции объединённого парсера.
+//функции сдвоенного парсера.
 struct Settings {
     char *url;
     int  port;
